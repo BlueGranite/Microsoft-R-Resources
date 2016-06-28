3cfcd491ae46fce23bab36e3ea42e70b1d1f10a1
David Eldersveld <deldersveld@blue-granite.com>
2016-06-28 10:16:58 -0400

    hdinsight r script

---
+++ hdinsight-r-server-spark/HDInsight.R
@@ -0,0 +1,30 @@
+####### Set Local Context #######
+rxSetComputeContext('local')
+
+####### Hadoop Command #######
+rxHadoopVersion()
+rxHadoopListFiles("/data", recursive = TRUE)
+
+####### Load income.csv from Hadoop #######
+incomepath <- file.path("/data/income/income.csv")
+hdfsFS <-RxHdfsFileSystem()
+incomedata <- RxTextData(incomepath, fileSystem = hdfsFS, stringsAsFactors = TRUE)
+rxGetVarInfo(incomedata)
+
+####### Create Logistic Regression Model #######
+incomeformula <- "IncBin ~ age + education + race + sex"
+model <- rxLogit(formula = incomeformula, data = incomedata)
+summary(model)
+
+
+####### Create Logistic Regression Model Using Local Parallel Context #######
+rxSetComputeContext('localpar')
+model <- rxLogit(formula = incomeformula, data = incomedata)
+summary(model)
+
+
+####### Create Logistic Regression Model Using Local Parallel Context #######
+spark <-RxSpark()
+rxSetComputeContext(spark)
+model <- rxLogit(formula = incomeformula, data = incomedata)
+summary(model)
